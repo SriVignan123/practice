7223a49f565ead6993b901eb4eb52cf1c83098a8
SriVignan123 <ss1138@g.rit.edu>
2025-02-11 11:05:05 +0400

    pushed to github

---
+++ practice.py
@@ -0,0 +1,47 @@
+"""
+
+def adding():
+
+    a=int(input('enter number 1:'))
+    b=int(input('enter number 2:'))
+    print(a+b)
+adding()
+
+def info():
+    n=input('enter name:')
+    d= input('enter discipline:')
+    e=input('enter emirate:')
+    print(n,d,e)
+info()
+
+def practice():
+    n=int(input('age in months:'))
+    y=int(input('enter number of das in a year:'))
+    p=input('enter name of your pet:')
+    s=float(input('enter first 5 digits of pi:'))
+    print('age in months:',n)
+    print('number of days in a year:',y)
+    print('name of pet:',p)
+    print('digits of pi:',s)
+practice()
+
+x=5
+y=3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(x//y)
+
+print('3'-'5')
+print('apple'+'juice')"""
+
+def promt_and_print():
+    a=int(input('enter number:'))
+    b=int(input('enter anoter number:'))
+    print('addition:',a+b)
+    print('subtraction',a-b)
+    print('multiplication',a * b)
+    print('division', a/b)
+promt_and_print()
